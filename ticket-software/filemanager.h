17da1becaee7659ec33c1acc7e2106947433794f
Whitehatcat <55483594+whitehatcat-JE@users.noreply.github.com>
2022-11-02 12:24:16 +1300

    Added structs for 4d projects db

--- ticket-software/filemanager.h
+++ ticket-software/filemanager.h
@@ -1,11 +1,47 @@
 #ifndef FILEMANAGER_H
 #define FILEMANAGER_H
 
+#include <QString>
+#include <QVector>
+
 
 class FileManager
 {
 public:
     FileManager();
+
+    struct Log {
+        int uniqueIdentifier;
+        int creationTime;
+        QString description;
+    };
+
+    struct Ticket {
+        int uniqueIdentifier;
+        int creationTime;
+        int priority;
+        QString title;
+        QString system;
+        QString progress;
+        QString description;
+        bool isOpen;
+
+        QVector<Log> logs;
+    };
+
+    struct Project {
+        int uniqueIdentifier;
+        QString name;
+        QString description;
+
+        QVector<Ticket> tickets;
+    };
+
+    QVector<Project> formatProjects(QString projectData) {
+        projectData = "0/,UntitledMMO/,An MMO/~0/,1667343548/,0/,Crash on load/,Save System/,Actively Worked On/,Game crashes when trying to save, big problem/,\
+                        1/,";
+    };
+
 };
 
 #endif // FILEMANAGER_H
