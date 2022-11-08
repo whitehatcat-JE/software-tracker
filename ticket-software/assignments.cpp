01e6f9900704059d2528757f2fcee95feb60ca3f
Whitehatcat <55483594+whitehatcat-JE@users.noreply.github.com>
2022-11-09 09:19:49 +1300

    Added project buttons to assignment tab

--- ticket-software/assignments.cpp
+++ ticket-software/assignments.cpp
@@ -29,12 +29,12 @@ void Assignments::on_ticketsButton_toggled(bool checked)
         for (int i = 0; i < 8; i++) {
             QPushButton *button = new QPushButton(this);
             button->setObjectName("displayedTicket" + QString::number(i));
-            button->setText("Hello World");
-            button->setStyleSheet("background-color: #32ACBE; color: white; height: 50px; font-family: Inter; font-size: 36px; font-weight: bold; text-align: left; padding-left: 10px;");
+            button->setText("Duplication exploit with shopkeeper NPCs");
+            button->setStyleSheet("background-color: #32ACBE; color: white; height: 50px; font-family: Inter; font-size: 24px; font-weight: bold; text-align: left; padding-left: 10px; border:none;");
 
             QLabel *label = new QLabel("metadata", button);
             label->setText("Created on: 9/12/22 - Physics Engine - Untitled MMO - Actively Working On");
-            label->setStyleSheet("font-family: Inter; font-size: 20px; text-align: right; margin-left:250px; font-weight: normal; vertical-align: middle;");
+            label->setStyleSheet("font-family: Inter; font-size: 18px; text-align: right; margin-left:550px; font-weight: normal; margin-top:16px;");
             ui->ticketsLayout->addWidget(button);
         }
     }
@@ -54,8 +54,22 @@ void Assignments::on_projectsButton_toggled(bool checked)
 {
     if (checked == false) {
         ui->projectsButton->setText("Projects                                                                                                           ►");
+        QObjectList list = ui->assignedItems->widget()->children();
+        for (int i = 0; i < list.size(); i++) {
+            QString name = list.at(i)->objectName();
+            if (name.contains("displayedProject")) {
+                delete list.at(i);
+            }
+        }
     } else {
         ui->projectsButton->setText("Projects                                                                                                           ▼");
+        for (int i = 0; i < 8; i++) {
+            QPushButton *button = new QPushButton(this);
+            button->setObjectName("displayedProject" + QString::number(i));
+            button->setText("Untitled MMO");
+            button->setStyleSheet("background-color: #32ACBE; color: white; height: 50px; font-family: Inter; font-size: 24px; font-weight: bold; text-align: left; padding-left: 10px; border:none;");
+            ui->projectsLayout->addWidget(button);
+        }
     }
 }
 
