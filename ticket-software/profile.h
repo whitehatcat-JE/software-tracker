dad2719821299a4d41f1ab012da4877a04a14d2f
SmurfPJ <smurfpj18@gmail.com>
2022-11-09 09:42:29 +1300

    Resolved github issues - Updated files

--- ticket-software/profile.h
+++ ticket-software/profile.h
@@ -2,13 +2,6 @@
 #define PROFILE_H
 
 #include <QWidget>
-#include <QMainWindow>
-//#include <QtSql>
-#include <QDebug>
-#include <QFileInfo>
-#include <QDialog>
-#include <QLineEdit>
-#include <QBoxLayout>
 
 namespace Ui {
 class Profile;
@@ -22,25 +15,8 @@ public:
     explicit Profile(QWidget *parent = nullptr);
     ~Profile();
 
-private slots:
-    //void on_Edit_clicked();
-    void on_Edit_clicked();
-
 private:
-
-
     Ui::Profile *ui;
-    QDialog* popup = new QDialog(this);
-    //Access Central widget on popup window
-    //Assign a vertical layout group
-    //Access central widget and use function
-    //cWidget->addWidget(nameField);
-    //cWidget->addWidget(timeZone);
-    //cWidget->addWidget(contactInfo);
-    QLineEdit* nameField = new QLineEdit(popup);
-    QLineEdit* timeZone = new QLineEdit(popup);
-    QLineEdit* ContactInfo = new QLineEdit(popup);
-
 };
 
 #endif // PROFILE_H
